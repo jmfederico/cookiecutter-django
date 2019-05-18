0d9345d9a556344206bc85376a47fde8a76418bc
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-05-18 15:06:21 -0500

    Keep required empty directories

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,15 @@
-import os
+import glob
 import urllib.request
+from pathlib import Path
 
 urllib.request.urlretrieve("https://www.gitignore.io/api/django,node", "../.gitignore")
 
-for file_name in os.listdir("_root"):
-    os.rename(f"_root/{file_name}", f"../{file_name}")
-os.rmdir("_root")
+# Move root files.
+_root = Path("_root")
+project_root = Path("..")
+for child in _root.iterdir():
+    child.rename(project_root / child.name)
+_root.rmdir()
+
+for path in glob.glob('../**/.cookiecutter-keep', recursive=True):
+    Path(path).unlink()
